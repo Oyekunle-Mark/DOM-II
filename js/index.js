d6f51e8a51d3d64f27eaa02da389bb0b14ba51a1
oyekunle-mark <oyekunlemac@gmail.com>
2019-05-14 12:46:00 +0100

    Faded the header image on hover using mouseove and mouseout events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+// fade and unfade the header image on mouseover and mouseout
+const headerImage  = document.querySelector('.intro img');
+headerImage.addEventListener('mouseover', function() {
+  this.style.opacity = '0.7';
+});
+
+headerImage.addEventListener('mouseout', function() {
+  this.style.opacity = '1';
+});
